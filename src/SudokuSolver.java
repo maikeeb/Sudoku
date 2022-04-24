a6a164a10eea04ab7a5174f4d959b1ed5a982224
Maikeeb <leaguehomey@gmail.com>
2022-04-24 18:54:17 -0400

    started solver which is going to use recursion

---
+++ src/SudokuSolver.java
@@ -0,0 +1,25 @@
+import sudoku.*;
+import BasicIO.*;
+
+
+public class SudokuSolver {
+
+    Sudoku sudoku;
+
+
+    public SudokuSolver(){
+        sudoku = new mySudoku();
+        sudoku.scramble();
+
+        solveSudoku();
+
+
+    }
+
+    public boolean solveSudoku(){
+
+
+
+
+    }
+}
